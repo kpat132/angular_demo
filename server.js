400701a18a09f14a95d22586b3843b268e2e1676
KrishnaP <kpat132@gmail.com>
2018-02-14 10:57:58 -1000

    part1-server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`)
+})
