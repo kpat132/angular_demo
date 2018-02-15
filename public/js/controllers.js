26fcae018f9d67cc4bbe53a25af73b0d811c3a18
KrishnaP <kpat132@gmail.com>
2018-02-15 09:54:11 -1000

    factory

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('myApp')
-.controller('myController', ['$scope','mainCharacter',function($scope, mainCharacter){
+.controller('myController', ['$scope','mainCharacter',`CharacterVersionFactory`,function($scope, mainCharacter, CharacterVersionFactory){
   $scope.myFirstName = 'krishna';
   $scope.myModel = 'Ready Player One'
   $scope.mainCharacter = mainCharacter;
+  $scope.versionedCharacter = CharacterVersionFactory.getCharacter();
+
 }]);
